ac0f8166dc6fab4ef416c60b524e4c848807f099
Juan David Ochoa Gaviria <jochoa.gaviria@hotmail.com>
2021-10-11 20:42:06 -0500

    First steps with datatypes

--- Scripts/basics.py
+++ Scripts/basics.py
@@ -9,3 +9,34 @@ x=12
 y=20
 z=1
 print (x,y,z)
+
+
+##Data types##
+x=10
+y="10"
+z=10.1
+
+sum1 = x+x
+sum2 = y+y
+
+print (sum1, sum2)
+print (type(x), type(y), type(z))
+##Data types##
+
+
+##List types##
+studentGrades = [9.1,8.8,7.5] ##this is a list
+print (f'Grades of a students: {studentGrades}')
+print (f"List of a diferents data types: {[33.1,10, 'a']}")
+print (f"A list into a list: {[3.5, 8, 'abc', ['Juan', 'David', 'Ochoa']]}")
+##List types##
+
+##Range##
+print(f"List of a range: {list(range(1,10))}")
+print(f"List of a range incresing by two: {list(range(1,10, 2))}")
+##Range##
+
+
+##Atributes##
+#you could use 'dir({datatype})' to see all atributes for a datatypes in python.
+##Atributes##

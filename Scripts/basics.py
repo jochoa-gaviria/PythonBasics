661450f34b99c0fe6210cf6f5d90e009c26c2b53
Juan David Ochoa Gaviria <jochoa.gaviria@hotmail.com>
2021-10-23 18:33:22 -0500

    Finish the third section

--- Scripts/basics.py
+++ Scripts/basics.py
@@ -39,4 +39,53 @@ print(f"List of a range incresing by two: {list(range(1,10, 2))}")
 
 ##Atributes##
 #you could use 'dir({datatype})' to see all atributes for a datatypes in python.
+# dir(__builtins__)
 ##Atributes##
+
+
+##Funtions##
+mysum = sum(studentGrades)
+length = len(studentGrades)
+mean = mysum / length
+print (f'the mean value is: {mean}')
+##Funtions##
+
+
+##Exercise - Calculate maximum##
+studentGrades = [9.1, 8.8, 7.5]
+max_value = max(studentGrades)
+print(f'The maximum grade is: {max_value}')
+##Exercise - Calculate maximum##
+
+
+##Exercise - count values##
+studentGrades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+print(f'the total of students with a grade of 10.0 is: {studentGrades.count(10.0)}')
+##Exercise - count values##
+
+##Exercise - lowerCase letters##
+username="Python3"
+print(f'{username} in lower case is: {username.lower()}')
+##Exercise - lowerCase letters##
+
+
+
+##Dictionaries##
+modayTemperature = [9.1, 8.8, 7.5]
+studentGrades = {"Marry":9.1, "Sim": 8.8, "John": 7.5}
+max_value = max(studentGrades.values())
+names = studentGrades.keys()
+print(f'The maximum grade of a dictionary is: {max_value}')
+##Dictionaries##
+
+##Tuples##
+mondayTemperature=(9.1,8.8)
+print (f'This is a tuple: {mondayTemperature}')
+##you cannot add values to an tuple. 
+##Tuples##
+
+
+##A dictionary with tuples##
+day_temperatures = {"morning": (38.4,36.0,37.3), "noon": (35.4,34.6,34.0), "evening": (31.0,32.1,30.8)}
+print(f'A dictionary with tuples {day_temperatures}')
+##A dictionary with tuples##

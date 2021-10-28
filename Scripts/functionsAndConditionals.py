c03f652acaa0b176e0f29d03a211b76e97ab713d
Juan David Ochoa Gaviria <jochoa.gaviria@hotmail.com>
2021-10-27 22:25:42 -0500

    Finish the section 5

--- Scripts/functionsAndConditionals.py
+++ Scripts/functionsAndConditionals.py
@@ -10,3 +10,32 @@ studentGrades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
 print(mean(studentGrades))
 print(mean(mondayTemperatures))
 
+
+
+##Exercise - Password controller##
+def password(pass1):
+    if len(pass1) >= 8:
+        return True
+    else:
+        return False
+##Exercise - Password controller##
+
+
+##Exercise - Temperature controller##
+def temperatureCalculate(temperature):
+    if temperature > 7:
+        return "Warm"
+    else:
+        return "Cold"
+##Exercise - Temperature controller##
+
+
+##Exercise Elif - Temperature controller##
+def temperatureCalc(temperature):
+    if temperature > 25:
+        return "Hot"
+    elif temperature >= 15 and temperature <= 25:
+        return "Warm"
+    else:
+        return "Cold"
+##Exercise Elif - Temperature controller##

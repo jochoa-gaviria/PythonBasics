223ec3510b41552050fab89ac9b407013811fe4e
Juan David Ochoa <jochoa.gaviria@hotmail.com>
2022-04-06 18:51:11 -0500

    it's complete the section 35 - dictionary, bonus app

--- Scripts/Dictionary/script.py
+++ Scripts/Dictionary/script.py
@@ -11,13 +11,30 @@ def GetDefinition(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     else:
-        closeWord = get_close_matches(word, data.keys())
+        closeWord = get_close_matches(word, data.keys(), cutoff=0.8)
         if closeWord:
-            return data[closeWord[0]]
+            userConfirmation = input("Did you mean <%s> instead? Enter Y if yes, or N if no: " % closeWord[0])
+            if userConfirmation == 'Y':
+                return data[closeWord[0]]      
+            elif userConfirmation=='N':
+                return "Word doesn't exists" 
+            else:
+                return "We didn't understand your entry"
+
         else:
             return "Word doesn't exists"
 
 
 word = input("Enter word: ")
-print(GetDefinition(word))
+output = GetDefinition(word)
+
+if isinstance(output, list):
+    for item in output:
+        print (item)
+else:
+    print(output)

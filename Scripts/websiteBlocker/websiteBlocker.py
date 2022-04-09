60ba93ce34dfc7abc06fc72390029c8cb97abf6b
Juan David Ochoa <jochoa.gaviria@hotmail.com>
2022-04-09 00:29:39 -0500

    finish section 36 - website blocker

--- Scripts/websiteBlocker/websiteBlocker.py
+++ Scripts/websiteBlocker/websiteBlocker.py
@@ -1,12 +1,26 @@
 import time
 from datetime import datetime as dt
-hostPath = 'etc/hosts'
+
+hostTemp = 'hosts'
+hostPath = '/private/etc/hosts'
 redirect="127.0.0.1"
 websiteList = ["www.facebook.com", "facebook.com"]
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
         print("Working hours...")
+        with open(hostPath, 'r+') as file :
+            content=file.read()
+            for website in websiteList:
+                if website not in content:
+                    file.write(f"{redirect} {website} \n")
     else:
         print("Fun hours...")
+        with open(hostPath, 'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in websiteList):
+                    file.write(line)
+            file.truncate()
     time.sleep(5)
